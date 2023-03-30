41fd8022efa26e1aed2cea7e4dc521d9346075c9
Paul Kussmaul <Paul.Kussmaul@mailbox.org>
2023-03-30 10:59:41 +0200

    behavior of view-buttons

---
+++ behavior-view-buttons.py
@@ -0,0 +1,40 @@
+# Topic: behavior of view-buttons
+
+import cadquery as cq
+
+part = cq.Workplane("XY").box(20,20,20)
+
+part = part.faces("<Z").workplane(centerOption="CenterOfMass").text("-Z",5,-1)
+part = part.faces(">Z").workplane(centerOption="CenterOfMass").text("+Z",5,-1)
+part = part.faces("<X").workplane(centerOption="CenterOfMass").text("-X",5,-1)
+part = part.faces(">X").workplane(centerOption="CenterOfMass").text("+X",5,-1)
+part = part.faces("<Y").workplane(centerOption="CenterOfMass").text("-Y",5,-1)
+part = part.faces(">Y").workplane(centerOption="CenterOfMass").text("+Y",5,-1)
+
+bottom = part.faces("<Z")
+top    = part.faces(">Z")
+left   = part.faces("<X")
+right  = part.faces(">X")
+front  = part.faces("<Y")
+back   = part.faces(">Y")
+
+show_object(part,name="part",options={"alpha":0.0,"color":(255,170,0)})
+
+#debug(bottom,name="bottom")
+#debug(top,name="top")
+#debug(left,name="left")
+#debug(right,name="right")
+debug(front,name="front")
+#debug(back,name="back")
+
+#rotated = bottom.rotate([0,0,0],[0,0,1],180)
+#show_object(rotated,name="rotated",options={"alpha":0.0,"color":(0,128,0)})
+
+# Behavior:
+# Top    (Shift F3): +Z
+# Bottom (Shift F4): Z-  !!! upside-down !!!
+# Front  (Shift F5): +Y  !!! back !!!
+# Back   (Shift F6): -Y  !!! front !!!
+# Left   (Shift F7): -X
+# Right  (Shift F8): +X
+
