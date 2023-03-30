15707c68e10e893e33cc97b4ccbb23fcd5787b21
Paul Kussmaul <Paul.Kussmaul@mailbox.org>
2023-03-30 13:49:46 +0200

    value of __name__

---
+++ whoami.py
@@ -0,0 +1,13 @@
+# Issue: value of __name__
+
+print(f"__name__ is {__name__}")
+
+if __name__ == '__main__':
+    print("running as main")
+
+# Behavior when run as main:
+# for VS-Code:    __name__ is "__main__"
+# for cq-editor:  __name__ is "temp"
+# Behavior when imported be runner.py:
+# for VS-Code:    __name__ is "anyscript"
+# for cq-editor:  __name__ is "anyscript"
